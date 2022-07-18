c4dfc35cf8ec7075bc2e07084d3b02b3fc14c88e
Ahtsham0715 <ahtsham50743@gmail.com>
2022-07-18 13:52:12 +0500

    sdk changes and design progress

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,24 +10,64 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+    _tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: customPrimaryColor,
+      floatingActionButton: _tabController.index == 0
+          ? FloatingActionButton.extended(
+              backgroundColor: Colors.deepPurple[400],
+              onPressed: () {},
+              label: customText(
+                  txt: ' File ',
+                  fsize: 18.0,
+                  // fweight: FontWeight.w300,
+                  clr: Colors.white),
+              icon: const Icon(
+                Icons.add,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            )
+          : null,
       drawer: Drawer(
         backgroundColor: customPrimaryColor,
         child: ListView(
           children: [
-            UserAccountsDrawerHeader(
-              accountName: customText(txt: 'Shami'),
-              accountEmail: customText(txt: 'abc@gmail.com'),
-              currentAccountPicture: const CircleAvatar(
-                backgroundColor: Colors.blueGrey,
-                child: Icon(
-                  Icons.person,
-                  size: 30.0,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: UserAccountsDrawerHeader(
+                accountName: customText(txt: 'Shami'),
+                accountEmail: customText(txt: 'abc@gmail.com'),
+                currentAccountPicture: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    size: 50.0,
+                    color: Colors.white60,
+                  ),
                 ),
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: customText(txt: 'Settings', fsize: 19.0),
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.white,
               ),
             ),
           ],
@@ -35,83 +75,111 @@ class _HomePageState extends State<HomePage> {
       ),
       body: DefaultTabController(
         length: 2,
-        child: CustomScrollView(slivers: [
-          SliverAppBar(
-            title: customText(txt: 'Share Clip'),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            backgroundColor: const Color.fromARGB(255, 35, 54, 65),
-            // pinned: true,
-            // floating: true,
-            // snap: true,
-            expandedHeight: responsiveHW(context, ht: 12),
-            collapsedHeight: responsiveHW(context, ht: 11),
-            flexibleSpace: const FlexibleSpaceBar(
-              collapseMode: CollapseMode.pin,
-            ),
-            bottom: const TabBar(
-              indicatorColor: Colors.blueGrey,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 3.0,
-              tabs: [
-                Tab(text: "History", icon: Icon(Icons.history)),
-                Tab(
-                  text: "Pinned",
-                  icon: Icon(Icons.push_pin_outlined),
-                ),
-              ],
-            ),
-          ),
-          // SliverStickyHeader(
-          SliverList(
-              delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              var docsnapshot = '';
-              return RefreshIndicator(
-                onRefresh: () async {
-                  setState(() {});
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 19, right: 19, top: 13),
-                  child: Column(
-                    children: ListTile.divideTiles(
-                      context: context,
-                      tiles: [
-                        ListTile(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0)),
-                          tileColor: const Color.fromARGB(255, 20, 35, 43),
-                          title: Text(
-                            docsnapshot.toString(),
-                            style: const TextStyle(
-                              fontSize: 17.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          // trailing: Icon(
-                          //   IconData(
-                          //     args['department_icon_code'],
-                          //     fontFamily: args['department_icon_fontfamily'],
-                          //     fontPackage: args['department_icon_fontpackage'],
-                          //   ),
-                          //   color: Colors.teal,
-                          //   size: 33,
-                          // ),
-                        ),
-                      ],
-                    ).toList(),
+        child: NestedScrollView(
+          headerSliverBuilder: (context, value) {
+            return [
+              SliverAppBar(
+                title: customText(
+                    txt: 'Share Clip', fsize: 22.0, fweight: FontWeight.w500),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
                   ),
                 ),
-              );
-            },
-            childCount: 5,
-          )),
-          // ),
-        ]),
+                backgroundColor: const Color.fromARGB(255, 35, 54, 65),
+                pinned: true,
+                floating: true,
+                snap: true,
+                centerTitle: true,
+                expandedHeight: responsiveHW(context, ht: 12),
+                collapsedHeight: responsiveHW(context, ht: 11),
+                flexibleSpace: const FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                ),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.devices_other,
+                        size: 28.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+                bottom: TabBar(
+                  controller: _tabController,
+                  indicatorColor: Colors.blueGrey,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorWeight: 3.0,
+                  tabs: const [
+                    Tab(text: "History", icon: Icon(Icons.history)),
+                    Tab(
+                      text: "Pinned",
+                      icon: Icon(Icons.push_pin_outlined),
+                    ),
+                  ],
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return RefreshIndicator(
+                    onRefresh: () async {
+                      setState(() {});
+                    },
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 19, right: 19, top: 13),
+                      child: Column(
+                        children: ListTile.divideTiles(
+                          context: context,
+                          tiles: [
+                            ListTile(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                              tileColor: const Color.fromARGB(255, 20, 35, 43),
+                              title: const Text(
+                                'd',
+                                style: TextStyle(
+                                  fontSize: 17.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              // trailing: Icon(
+                              //   IconData(
+                              //     args['department_icon_code'],
+                              //     fontFamily: args['department_icon_fontfamily'],
+                              //     fontPackage: args['department_icon_fontpackage'],
+                              //   ),
+                              //   color: Colors.teal,
+                              //   size: 33,
+                              // ),
+                            ),
+                          ],
+                        ).toList(),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              // 2nd tab
+              const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       // darkTheme: ThemeData(
       //   brightness: Brightness.dark,
       //   primarySwatch: Colors.blue,
