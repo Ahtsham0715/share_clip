7bf0bf03788fb1f7e93d9dad1197989885e4eca3
Ahtsham0715 <ahtsham50743@gmail.com>
2022-07-20 19:27:15 +0500

    changes in signin page and image folder

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 import 'package:share_clip/custom%20widgets/custom_widgets.dart';
+import 'package:share_clip/home_page.dart';
 
 class SigninPage extends StatefulWidget {
   const SigninPage({Key? key}) : super(key: key);
@@ -25,7 +27,7 @@ class _SigninPageState extends State<SigninPage> {
       body: ListView(
         children: [
           SizedBox(
-            height: MediaQuery.of(context).size.height * 0.05,
+            height: MediaQuery.of(context).size.height * 0.1,
           ),
           Image.asset(
             'assets/images/signin.png',
@@ -36,7 +38,7 @@ class _SigninPageState extends State<SigninPage> {
           ),
           Padding(
             padding: const EdgeInsets.all(15.0),
-            child: TextButton.icon(
+            child: TextButton(
               style: ButtonStyle(
                 padding: MaterialStateProperty.all(
                   const EdgeInsets.all(10.0),
@@ -46,17 +48,26 @@ class _SigninPageState extends State<SigninPage> {
                 ),
                 backgroundColor: MaterialStateProperty.all(Colors.teal),
               ),
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.google,
-                size: 25.0,
-                color: Colors.red,
+              onPressed: () {
+                Get.to(
+                  () => const HomePage(),
+                );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/google.png',
+                    fit: BoxFit.fill,
+                    height: MediaQuery.of(context).size.height * 0.04,
+                  ),
+                  customText(
+                      txt: 'Signin with Google',
+                      fsize: 20.0,
+                      fweight: FontWeight.w500,
+                      clr: Colors.white),
+                ],
               ),
-              label: customText(
-                  txt: 'Signin with Google',
-                  fsize: 19.0,
-                  fweight: FontWeight.w500,
-                  clr: Colors.white),
             ),
           ),
         ],
