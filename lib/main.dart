94ab814e8b6ce4137490b29d67a027d9a1fbdadf
Ahtsham0715 <ahtsham50743@gmail.com>
2022-07-21 20:56:04 +0500

    google signing package added

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:share_clip/custom%20widgets/custom_widgets.dart';
 import 'package:share_clip/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
--- lib/signin.dart
+++ lib/signin.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:share_clip/custom%20widgets/custom_widgets.dart';
 import 'package:share_clip/home_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SigninPage extends StatefulWidget {
   const SigninPage({Key? key}) : super(key: key);
@@ -12,10 +14,29 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         elevation: 0.0,
         title: customText(
           txt: 'SignIn',
@@ -50,9 +71,10 @@ class _SigninPageState extends State<SigninPage> {
                     MaterialStateProperty.all(Colors.teal.withAlpha(100)),
               ),
               onPressed: () {
-                Get.to(
-                  () => const HomePage(),
-                );
+                // Get.to(
+                //   () => const HomePage(),
+                // );
+                signInWithGoogle();
               },
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
