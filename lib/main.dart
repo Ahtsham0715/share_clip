7b3cdc658e4d12a66e2b828f8f7168ed07a4ec3e
Ahtsham0715 <ahtsham50743@gmail.com>
2022-07-23 22:35:37 +0500

    notification added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,41 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:share_clip/custom%20widgets/custom_widgets.dart';
+import 'package:share_clip/notifications.dart';
 import 'package:share_clip/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:workmanager/workmanager.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(
+
+      // The top level function, aka callbackDispatcher
+      callbackDispatcher,
+
+      // If enabled it will post a notification whenever
+      // the task is running. Handy for debugging tasks
+      isInDebugMode: true);
+  // Periodic task registration
+  Workmanager().registerPeriodicTask(
+    "2",
+
+    //This is the value that will be
+    // returned in the callbackDispatcher
+    "simplePeriodicTask",
+
+    // When no frequency is provided
+    // the default 15 minutes is set.
+    // Minimum frequency is 15 min.
+    // Android will automatically change
+    // your frequency to 15 min
+    // if you have configured a lower frequency.
+    frequency: const Duration(minutes: 15),
+  );
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
---
+++ lib/notifications.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:workmanager/workmanager.dart';
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    // initialise the plugin of flutterlocalnotifications.
+    FlutterLocalNotificationsPlugin flip = FlutterLocalNotificationsPlugin();
+
+    // app_icon needs to be a added as a drawable
+    // resource to the Android head project.
+    var android = const AndroidInitializationSettings('@mipmap/ic_launcher');
+    // var IOS = const IOSInitializationSettings();
+
+    // initialise settings for both Android and iOS device.
+    var settings = InitializationSettings(android: android);
+    flip.initialize(settings);
+    _showNotificationWithDefaultSound(flip);
+    return Future.value(true);
+  });
+}
+
+Future _showNotificationWithDefaultSound(flip) async {
+  // Show a notification after every 15 minute with the first
+  // appearance happening a minute after invoking the method
+  var androidPlatformChannelSpecifics =
+      const AndroidNotificationDetails('your channel id', 'your channel name',
+          // 'your channel description',
+          importance: Importance.max,
+          priority: Priority.high);
+  // var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+
+  // initialise channel platform for both Android and iOS device.
+  var platformChannelSpecifics = NotificationDetails(
+    android: androidPlatformChannelSpecifics,
+    // iOS: iOSPlatformChannelSpecifics
+  );
+  await flip.show(
+      0,
+      'Sync Data',
+      'Sync latest clipboard data across connected devices',
+      platformChannelSpecifics,
+      payload: 'Default_Sound');
+}
